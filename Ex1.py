044cb7dd1f9571bf61421b96de097a91e583ce32
unknown <kauamarcondes002@gmail.com>
2026-02-24 09:43:12 -0300

    Exercicios de aula

---
+++ Ex1.py
@@ -0,0 +1,6 @@
+number = float(input("Digite um numero"))
+
+if (number >100):
+    print("Número muito grande, tem que ser menor que 100")
+else: 
+    print ((number/2), "Essa é a metade do seu numero")
---
+++ Ex3.py
@@ -0,0 +1,17 @@
+number = float(input("Digite o número 1 "))
+number2 = float(input("Digite o número 2 "))
+
+operator = input("O que você quer fazer ? /, *, +, -")
+
+if (operator == "/"):
+     
+     print (number/number2, "Essa é a dvisão dos numeros")
+
+if operator == "*":
+      print (number*number2, "Essa é a multiplicação dos numeros")
+
+if operator == "+":
+       print (number+number2, "Essa é a soma dos numeros")
+
+if operator == "-":
+       print (number-number2, "Essa é a subtração dos numeros")
