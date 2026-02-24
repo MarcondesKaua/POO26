2bfec45a1d28a43cfea8b5b290ec8f8c4d7d6fcf
unknown <kauamarcondes002@gmail.com>
2026-02-24 09:30:22 -0300

    Alteração basica na logica de comparaçaão

--- Zottesso.py
+++ Zottesso.py
@@ -1,15 +1,15 @@
 name = input("Qual seu nome: ")
 age = int(input("Idade: "))
-payment = float(input("Qual seu salario"))
+payment = float(input("Qual seu salario: "))
 
-if (payment <= 1000):
-    print("Olá", name, " Você tem", age, "Seu salario de R$: ", payment, " é muito ruim")
+if (payment <= 3000):
+    print("Olá", name, "Você tem", age, "Seu salario de R$: ", payment, " é muito ruim")
 
-elif (payment >1000 and payment <= 10000): 
-    print("Olá", name, " Você tem", age, "Seu salario de R$: ", payment, " é decente")
+elif (payment <= 10000): 
+    print("Olá", name, "Você tem", age, "Seu salario de R$: ", payment, " é decente")
 
-elif (payment > 10000 and payment < 20000) :
-    print("Olá", name, " Você tem", age, "Seu salario de R$: ", payment, " é normal, da para pagar unimed")
+elif (payment< 20000) :
+    print("Olá", name, "Você tem", age, "Seu salario de R$: ", payment, " é normal, da para pagar unimed")
 
 else:
-    print("Olá", name, " Você tem", age, "Seu salario de R$: ", payment, " é normal, bem vindo Jeff Bezos")
+    print("Olá", name, "Você tem", age, "Seu salario de R$: ", payment, " é normal, bem vindo Jeff Bezos")
